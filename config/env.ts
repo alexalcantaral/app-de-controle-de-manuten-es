b79cd2c0fad9a464fcbee172f8d5d0dca1a17a41
alexalcantaral <alexalcantara280@gmail.com>
2026-08-11 09:53:54 -0300

    chore: adiciona arquivos de configuração do projeto

---
+++ config/env.ts
@@ -0,0 +1,11 @@
+/**
+ * Configuração de ambiente do aplicativo.
+ *
+ * IMPORTANTE: ajuste o host conforme onde o app está rodando:
+ *  - Emulador Android:      http://10.0.2.2:5000
+ *  - Dispositivo físico:    http://<IP-da-sua-máquina-na-rede>:5000
+ *  - iOS simulator / web:   http://localhost:5000
+ */
+export const API_HOST = "http://192.168.0.13:5000";
+
+export const API_URL = `${API_HOST}/api`;
